b3f33a03c3e09006fce5987b4674be512e5912d0
IgorLadnik <ladnik@hotmail.com>
2019-09-14 06:02:28 +0300

    Change in path

--- _start/k.cmd
+++ _start/k.cmd
@@ -1,2 +1,2 @@
-cd C:\kafka_2.12-2.3.0\_start
-..\bin\windows\kafka-server-start.bat ..\config\server.properties
+cd C:\kafka_2.12-2.3.0
+.\bin\windows\kafka-server-start.bat .\config\server.properties
