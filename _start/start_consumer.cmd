46d0c62c06e0f68505be4b7df7d4aa8f7c776aea
IgorLadnik <ladnik@hotmail.com>
2019-09-14 06:05:40 +0300

    Change in path

--- _start/start_consumer.cmd
+++ _start/start_consumer.cmd
@@ -1,2 +1,2 @@
-cd C:\kafka_2.12-2.3.0\_start
-..\bin\windows\kafka-console-consumer.bat --bootstrap-server localhost:9092 --topic %1 --from-beginning
+cd C:\kafka_2.12-2.3.0
+.\bin\windows\kafka-console-consumer.bat --bootstrap-server localhost:9092 --topic %1 --from-beginning
