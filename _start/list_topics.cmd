74cb825d29ff9705ccf4077709dcbb20e4b1d87b
IgorLadnik <ladnik@hotmail.com>
2019-09-14 06:06:28 +0300

    Change in path

--- _start/list_topics.cmd
+++ _start/list_topics.cmd
@@ -1,2 +1,2 @@
-cd C:\kafka_2.12-2.3.0\_start
-..\bin\windows\kafka-topics.bat --list --zookeeper localhost:2181
+cd C:\kafka_2.12-2.3.0
+.\bin\windows\kafka-topics.bat --list --zookeeper localhost:2181
