4ad58226699d233390fe9629bc1a35d2fcd578a0
IgorLadnik <ladnik@hotmail.com>
2019-09-14 06:06:49 +0300

    Update send_message.cmd

--- _start/send_message.cmd
+++ _start/send_message.cmd
@@ -1,2 +1,2 @@
-cd C:\kafka_2.12-2.3.0\_start
-..\bin\windows\kafka-console-producer.bat --broker-list localhost:9092 --topic %1
+cd C:\kafka_2.12-2.3.0
+.\bin\windows\kafka-console-producer.bat --broker-list localhost:9092 --topic %1
